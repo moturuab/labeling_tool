145bdf07487c557a3de048cc3441b96f6516ebca
Your Name <abhishek.moturu@mail.utoronto.ca>
2023-11-06 12:47:28 -0500

    keep zoom

--- scroller.py
+++ scroller.py
@@ -227,7 +227,7 @@ class IndexTracker(object):
             self.im.set_data(self.X[:, :, self.ind])
             self.im.set_cmap('gray')
             self.im.set_clim(vmin=0)
-            self.im.set_clim(vmax=1)
+            self.im.set_clim(vmax=np.max(self.X[:,:,self.ind]))
             #self.mask = self.masks[self.ind] #self.Y[:, :, self.ind]
             #s = 1*(np.min(self.Y[:,:,self.ind]) + np.max(self.Y[:,:,self.ind]))/2
             #masked = np.ma.where(self.mask > 3*np.mean(self.mask), 1, 0)
