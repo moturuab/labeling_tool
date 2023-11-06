389a14628d7fb7ef06253490616994c39921bf69
Your Name <abhishek.moturu@mail.utoronto.ca>
2023-11-06 00:32:38 -0500

    keep zoom

--- scroller.py
+++ scroller.py
@@ -190,7 +190,7 @@ class IndexTracker(object):
             masked = np.ma.where(mask > 3 * np.mean(mask), 1, 0)
             masked = np.ma.masked_where(masked == 0, masked)
             self.masks.append(masked)
-        self.im = ax.imshow(self.ims[self.ind], cmap='gray', vmin=0, vmax=1)
+        ax.imshow(self.ims[self.ind], cmap='gray', vmin=0, vmax=1)
         #self.mask =
         #self.im = ax.imshow(self.X[:, :, self.ind], cmap='gray', vmin=0, vmax=1)
         #self.mask = self.Y[:, :, self.ind]
@@ -223,15 +223,14 @@ class IndexTracker(object):
     def update(self):
         if not DONE:
             #self.ax.cla()
-            self.im.set_data(self.ims[self.ind], cmap='gray', vmin=0, vmax=1)
+            ax.imshow(self.ims[self.ind], cmap='gray', vmin=0, vmax=1)
             #self.im.set_data(self.X[:, :, self.ind])
             #self.mask = self.masks[self.ind] #self.Y[:, :, self.ind]
             #s = 1*(np.min(self.Y[:,:,self.ind]) + np.max(self.Y[:,:,self.ind]))/2
             #masked = np.ma.where(self.mask > 3*np.mean(self.mask), 1, 0)
             #masked = np.ma.masked_where(masked == 0, masked)
             #self.im.set_data(masked)
-            self.im.set_data(self.masks[self.ind], cmap='bwr', interpolation='none', alpha=OPACITY, vmin=0, vmax=1)
-            #self.im = ax.imshow(self.masks[self.ind], cmap='bwr', interpolation='none', alpha=OPACITY, vmin=0, vmax=1)
+            self.im.set_data(self.masks[self.ind]) #, cmap='bwr', interpolation='none', alpha=OPACITY, vmin=0, vmax=1)
             #self.mask.set_data(self.Y[:, :, self.ind])
 
             for (point, circ) in zip(self.points, self.circles):
